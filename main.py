58e15d89f920c721119cd687d1ef432ef862d666
Misenpai <misenpai200231@gmail.com>
2023-09-19 16:53:46 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/edit',methods=["GET","POST"])
+def edit():
+    if request.method=="POST":
+        return "POST request is here"
+    return render_template('index.html')
+
+app.run(debug=True)
